c234e21d1db04871ed192615bce5864dc1eaabe6
EdwardMwaura <edduemwaurer@outlook.com>
2019-08-23 15:47:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-asha1w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-asha1w)
---
+++ index.ts
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+/* function adjacentElementsProduct(inputArray: number[]): number {
+
+} */
+
+//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
