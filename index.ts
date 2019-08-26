8da4b44ff739f546f981266fd4c4fe4d96b67dcb
EdwardMwaura <edduemwaurer@outlook.com>
2019-08-26 12:34:13 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -5,8 +5,18 @@ import './style.css';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>Algorithm challenge 5</h1>`;
 
-/* function adjacentElementsProduct(inputArray: number[]): number {
+function adjacentElementsProduct(inputArray: number[]): number {
+  let prod = inputArray[0] * inputArray[1];
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    const product = inputArray[i] * inputArray[i + 1];
+    if (prod < product) {
+      prod = product;
+    } else {
+      prod;
+    }
+  }
+  return prod;
 
-} */
+}
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3,]));
